016ed3704c83d2ae4b33578d53785a4bbc36cd4c
TKGAP <223621747+TKGAP@users.noreply.github.com>
2025-07-30 20:48:50 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open('v4').then(cache => cache.addAll([
+    caches.open('v5').then(cache => cache.addAll([
       './index.html',
       './manifest.json',
       './icon.png'
