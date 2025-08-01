257a52d0b6256d833985de04320b9d58b446851b
TKGAP <223621747+TKGAP@users.noreply.github.com>
2025-08-01 11:23:35 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open('task-app-v3').then(cache => {
+    caches.open('task-app-v4').then(cache => {
       return cache.addAll([
         './',
         './index.html'
@@ -9,10 +9,23 @@ self.addEventListener('install', e => {
   );
 });
 
+// 新しいSWが来たら即座に適用
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== 'task-app-v4') {
+            return caches.delete(key);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(response => {
-      return response || fetch(e.request);
-    })
+    fetch(e.request).catch(() => caches.match(e.request))
   );
 });
